413554928185c96ae5fc8d637faee79ee4ac40bc
dvdgamer <davidnmponte@gmail.com>
2025-06-16 14:11:16 +0200

    update: enhance get_next_line with dynamic stash allocation, implement utility functions, and refine buffer management This is still broken and will seg fault

--- get_next_line.h
+++ get_next_line.h
@@ -1,20 +1,17 @@
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# define BUFFER_SIZE 42
+# define MAX_FD 1024
 
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 char	*get_next_line(int fd);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_calloc(size_t count, size_t size);
 size_t	ft_strlen(const char *str);
-size_t	ft_strlcpy(char *dest, const char *src, size_t n);
-size_t	ft_strlcat(char *dest, const char *src, size_t destsize);
+char    *ft_strdup(const char *s);
+char    *ft_strchr(const char *str, int c);
+char    *ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -21,3 +21,64 @@ size_t	ft_strlen(const char *str)
                 i++;
         return (i);
 }
+
+char    *ft_strdup(const char *s)
+{
+        char    *returned_str;
+        size_t  i;
+        size_t  len;
+
+        len = ft_strlen(s);
+        returned_str = malloc(len + 1);
+        if (!returned_str)
+                return (NULL);
+        i = 0;
+        while (i < len)
+        {
+                returned_str[i] = s[i];
+                i++;
+        }
+        returned_str[i] = '\0';
+        return (returned_str);
+}
+
+char    *ft_strchr(const char *str, int c)
+{
+        unsigned char   ch;
+
+        ch = (unsigned char)c;
+        while (*str != ch)
+        {
+                if (*str == '\0')
+                        return (NULL);
+                str++;
+        }
+        return ((char *) str);
+}
+
+char    *ft_strjoin(char const *s1, char const *s2)
+{
+        size_t  i;
+        char    *return_string;
+
+        if (ft_strlen(s1) + ft_strlen(s2) < 1)
+                return (ft_strdup(""));
+        return_string = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+        if (!return_string)
+                return (NULL);
+        i = 0;
+        while (i < ft_strlen(s1))
+        {
+                return_string[i] = s1[i];
+                i++;
+        }
+        i = 0;
+        while (i < ft_strlen(s2))
+        {
+                return_string[ft_strlen(s1) + i] = s2[i];
+                i++;
+        }
+        return_string[ft_strlen(s1) + i] = '\0';
+        return (return_string);
+}
+

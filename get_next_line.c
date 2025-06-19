e4617110c876d61c7d19b8b684d6ac03d7f4e6fa
dvdgamer <davidnmponte@gmail.com>
2025-06-19 16:47:28 +0200

    update: enhance get_next_line implementation with improved buffer handling and memory management

--- get_next_line.c
+++ get_next_line.c
@@ -18,91 +18,77 @@
 
 char	*get_next_line(int fd)
 {
-	char			*buffer;
-	static char		*line_break;
-	static char		*stash;
-	size_t 			len;
+	char		*buffer;
+	char		*line_break;
+	char		*result;
+	static char	*stash;
+	size_t 		len;
 	char		*tmp;
-	size_t	byte_count;
+	size_t		byte_count;
 
 	buffer = calloc(BUFFER_SIZE + 1, sizeof(char));
-	tmp = calloc(BUFFER_SIZE + 1, sizeof(char));
 
 	if (stash == NULL)
 		stash = ft_strdup("");
-	else
+
+	line_break = ft_strchr(stash, '\n');
+	if (line_break)
 	{
-		if (ft_strchr(stash, '\n'))
-		{
-			line_break = ft_strchr(stash, '\n');
-			printf("line_break1:%s\n", line_break + 1);
-			len = line_break - buffer;
-			printf("len:%ld\n", len);
-			tmp = ft_substr(buffer, 0, len);
-			printf("tmp1:%s\n", tmp);
-			stash = line_break + 1;
-			return (tmp);
-		}
+		len = (line_break + 1) - stash;
+		tmp = ft_substr(stash, 0, len);
+		stash = ft_strdup(line_break + 1);
+		return (tmp);
 	}
 
+	//TODO: Add null check
+
+	/* printf("stash before buffer is read:%s\n",stash); */
 	byte_count = read(fd, buffer, BUFFER_SIZE);
-	if (byte_count == 0)
+	/* printf("Original buffer:%s\n", buffer); */
+	if (byte_count < BUFFER_SIZE)
 	{
 		stash = ft_strjoin(stash, buffer);
-		free(buffer);
-		free(tmp);
 		return (stash);
 	}
 
 	line_break = ft_strchr(buffer, '\n');
-	printf("\nbuffer:%s\n", buffer);
 
 	if (line_break)
 	{
-		line_break++;
-		/* printf("\nline_break: %s\n", newline); */
-		tmp = ft_strjoin(tmp, buffer - *line_break);
-		printf("buffer - *line_break:%s\n", line_break - *buffer);
-		stash = line_break;
-		printf("tmp2:%s\n", tmp);
-		printf("stash:%s\n", stash);
-		return (tmp);
+		len = (line_break + 1) - buffer;
+		tmp = ft_substr(buffer, 0, len);
+		result = ft_strjoin(stash, tmp);
+		free(tmp);
+		stash = ft_strdup(line_break + 1);
+		return (result);
 	}
 	else
 	{
-		while (line_break == NULL)
+		while (line_break == NULL && byte_count == BUFFER_SIZE)
 		{	
-			if (byte_count == 0)
-			{
-				stash = ft_strjoin(stash, buffer);
-				free(buffer);
-				free(tmp);
-				return (stash);
-			}
-			line_break = ft_strchr(buffer, '\n');
-			if (line_break == NULL)
-			{
-				stash = ft_strjoin(stash, buffer);
-			}
-			else
-			{
-				// TODO: Add to stash only until the line break
-				stash = ft_strjoin(stash, buffer - (*line_break + 1));
-				if(ft_strchr(stash, '\n') == NULL)
-					return (stash);
-				else
-				{
-					tmp = ft_strjoin(tmp, line_break + 1); //save it to stash
-					ft_strjoin(stash, tmp);
-					return (tmp);
-				}
-				//TODO: need to check if there's any \n in stash already
-				/* stash = ft_strjoin(stash, buffer); */
-			}
+			stash = ft_strjoin(stash, buffer);
+			free(buffer);
 			byte_count = read(fd, buffer, BUFFER_SIZE);
+			if (byte_count <= 0)
+			{
+				stash = ft_strjoin(stash, buffer);
+				return stash;
+			};
+			line_break = ft_strchr(buffer, '\n');
 		}
+		
+		// If there's a line break
+		len = (line_break + 1) - buffer;
+		tmp = ft_substr(buffer, 0, len);
+		result = ft_strjoin(stash, tmp);
+		stash = ft_strdup(line_break + 1);
+		return (result);
 	}
-	printf("stash2:%s\n", stash);
 	free (buffer);
+	if (stash)
+	{
+		free(stash);
+		stash = NULL;
+	}
 	return (stash);
 }
--- main.c
+++ main.c
@@ -8,15 +8,15 @@ int main(void)
 	char	*next_line;
 	int		count;
 
-	count = 0;
+	count = 1;
 	fd = open("test.txt", O_RDONLY);
-	while (count < 7)
+	while (count < 9)
 	{
 		next_line = get_next_line(fd);
 		if (next_line == NULL)
 			break ;
 		count++;
-		printf("------------------------\n");
+		printf("\n------------------------\n");
 		printf("[%d]:%s\n", count, next_line);
 		next_line = NULL;
 	}

f9521e89902b2656cd5d47e5126952e6fe400ef9
David Ponte <dponte@f1r2s9.codam.nl>
2025-07-05 12:08:51 +0200

    feat:function works now. refactored the function and deleted unused variables

--- get_next_line.c
+++ get_next_line.c
@@ -16,53 +16,42 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-char *get_result(char **stash, char *buffer, char *line_break)
+char	*get_result(char **stash, char *line_break)
 {
 	size_t		len;
 	char		*result;
-	char		*tmp;
 
-	len = (line_break + 1) - buffer;
-	tmp = ft_substr(buffer, 0, len);
-	result = ft_strjoin(*stash, tmp);
+	len = (line_break + 1) - *stash;
+	result = ft_substr(*stash, 0, len);
 	*stash = ft_strdup(line_break + 1);
 	return (result);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	char		*buffer;
 	char		*result;
 	char		*line_break;
-	int			len;
-	static char *stash = NULL;
+	static char	*stash = NULL;
 	size_t		byte_count;
-	
+
 	buffer = calloc(BUFFER_SIZE, sizeof(char));
 	line_break = NULL;
-
 	if (stash == NULL)
 		stash = ft_strdup("");
 	else
 	{
-		if (ft_strchr(stash, '\n'))
+		if (ft_strchr(stash, '\n') != NULL)
 		{
 			line_break = ft_strchr(stash, '\n');
-			len = (line_break + 1) - stash;
-			result = ft_substr(stash, 0, len);
-			stash = ft_strdup(line_break + 1);
-			printf("stash:%s\n", stash);
-			return (result);
+			return (get_result(&stash, line_break));
 		}
 	}
-
 	while (line_break == NULL)
-	{ // ft_read_and_add_to_stash()
+	{
 		byte_count = read(fd, buffer, BUFFER_SIZE);
-		line_break = ft_strchr(buffer, '\n');
 		stash = ft_strjoin(stash, buffer);
+		line_break = ft_strchr(stash, '\n');
 	}
-	// Once it finds the \n
-	return (get_result(&stash, buffer, line_break));
+	return (get_result(&stash, line_break));
 }
-

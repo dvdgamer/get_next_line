2478f9c411038a2fa07dfe539dfdd034f6b387d7
David Ponte <dponte@f0r6s5.codam.nl>
2025-07-07 16:38:23 +0200

    feat: proper declaration of BUFFER_SIZE and FD_MAX in header file

--- get_next_line.h
+++ get_next_line.h
@@ -13,10 +13,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
-# define MAX_FD 1024
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
 
 # include <stdint.h>
 # include <stddef.h>
@@ -24,6 +26,8 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
+void	*ft_calloc(size_t count, size_t size);
+char	*get_result(char **stash, char *line_break);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *str, int c);

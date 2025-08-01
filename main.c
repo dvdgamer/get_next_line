0f21cb0e12a9809eba50c938a2324e8720cb4ed4
David Ponte <dponte@f1r2s13.codam.nl>
2025-08-01 11:00:40 +0200

    added another test file to test an edge case

--- main.c
+++ main.c
@@ -26,5 +26,24 @@ int main(void)
 	next_line = NULL;
 
 	close(fd);
+
+	count = 1;
+	fd = open("multiple_line_no_nl.txt", O_RDONLY);
+	/* while (count < 10) */
+	while (1)
+	{
+		next_line = get_next_line(fd);
+		if (next_line == NULL)
+			break ;
+		printf("\n------------------------\n");
+		printf("[%d]:%s\n", count, next_line);
+		count++;
+		free (next_line);
+		next_line = NULL;
+	}
+	free (next_line);
+	next_line = NULL;
+
+	close(fd);
 	return (0);
 }
